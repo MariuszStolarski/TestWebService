4096f0c5c0a9ef7a0fed6cae4795883af8ebae50
mast <mariusz.stolarski@mobica.com>
2018-06-12 13:48:27 +0200

    Section 11: some further correction to the JWT usage

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import JWT, jwt_required
+from flask_jwt_extended import jwt_required
 
 from models.item import ItemModel
 
@@ -18,7 +18,7 @@ class Item(Resource):
 		required=True, 
 		help="Every itme needs a store id!")
 
-	@jwt_required() # to require authenticaiton via JWT
+	@jwt_required # to require authenticaiton via JWT
 	def get(self, name):
 
 		item = ItemModel.find_by_name(name)
