147294d7f6dab53353b64427c8774ca8bad1a41a
mast <mariusz.stolarski@mobica.com>
2018-06-13 10:07:29 +0200

    Allowing solution to work on local host

--- app.py
+++ app.py
@@ -38,4 +38,9 @@ api.add_resource(UserLogin, '/login')
 if __name__ == '__main__':
 	from db import db
 	db.init_app(app)
+
+	@app.before_first_request
+	def create_tables():
+		db.create_all()
+
 	app.run(port=5000, debug = True)
